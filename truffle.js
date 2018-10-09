313d0ba301aa55e15e2059b494a4c6dad4b8034b
Gavin <gavin@carbon.money>
2018-10-08 21:26:44 -0400

    removed package json lock and added truffle hd wallet

--- truffle.js
+++ truffle.js
@@ -1,4 +1,47 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+require('dotenv').config();
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+    networks: {
+        // Test RPC environment
+        development: {
+            host: "localhost",
+            port: 8545,
+            network_id: "*", //Listen to all networks,
+            gasLimit: 6000000
+        },
+        coverage: {
+            host: "localhost",
+            network_id: "*",
+            port: 8555, // <-- If you change this, also set the port option in .solcover.js.
+            gas: 0xfffffffffff, // <-- Use this high gas value
+            gasPrice: 0x01 // <-- Use this low gas price
+        },
+        rinkeby: {
+            provider: function () {
+                return new HDWalletProvider(process.env.MNEMONIC, "https://rinkeby.infura.io/v3/" + process.env.INFURA_API_KEY)
+            },
+            network_id: 4,
+        },
+        ropsten: {
+            provider: function () {
+                return new HDWalletProvider(process.env.MNEMONIC, "https://ropsten.infura.io/v3/" + process.env.INFURA_API_KEY_MAIN)
+            },
+            network_id: 3,
+        },
+        mainnet: {
+            provider: function () {
+                return new HDWalletProvider(process.env.MNEMONIC, "https://mainnet.infura.io/v3/" + process.env.INFURA_API_KEY_MAIN)
+            },
+            network_id: 1,
+            gas: 7000000, // Largest contracts can take almost 7mm gas
+            gasPrice: 35000000000, // 35 GWei, should be very fast: https://ethgasstation.info/
+        }
+    },
+    solc: {
+        optimizer: { // deployed Sep18 { enabled: true, runs: 200 }
+            enabled: true,
+            runs: 200
+        }
+    }
 };
